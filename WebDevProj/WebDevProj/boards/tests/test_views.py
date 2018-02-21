c48e9d44e55e2562d9a70abaef403597076771b7
dylaann <C00204772@itcarlow.ie>
2018-02-21 12:30:23 +0000

    Basic Web Forum

---
+++ WebDevProj/WebDevProj/boards/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.urls import resolve, reverse
+from django.test import TestCase
+from ..views import TopicListView
+from django.contrib.auth.models import User
+from ..forms import NewTopicForm
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+
+@login_required
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = request.user  # <- here
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=request.user  # <- and here
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
